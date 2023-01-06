3bcc8fb5f6dac790817bf2ab60faa1e045abcd6a
Agnieszka Wiekiera <agnieszka.wiekiera97@gmail.com>
2023-01-06 13:47:34 +0100

    poprawki

--- mvnProject/src/main/java/org/example/Calculator.java
+++ mvnProject/src/main/java/org/example/Calculator.java
@@ -3,42 +3,19 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    public static void main(String[] args){
-        char operator;
-        double value1;
-        double value2;
-        double result;
+    public static int getSum(int a, int b) {
+        			return a + b;
+        		}
+    public static int getSubtraction(int a, int b) {
+        			return a - b;
+        		}
 
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Wybierz rodzaj działania: +, -, *, /");
-        operator = input.next() .charAt(0);
-
-        System.out.println("Wpisz pierwszą liczbę");
-        value1 = input.nextDouble();
-
-        System.out.println("Wpisz drugą liczbę");
-        value2 = input.nextDouble();
-
-        calculate(value1 ,value2, operator);
-
+    public static int getMultiplication(int a, int b) {
+        return a * b;
     }
-    public static double calculate(double value1, double value2, char operator){
-       double response = 0;
-       if (operator == '+'){
-           response = value1 + value2;
-       }
-       else if (operator == '-'){
-           response = value1 - value2;
-       }
-       else if (operator == '*') {
-           response = value1 * value2;
-       }
-       else if (operator == '/'){
-           response = value1 / value2;
-       }
-       System.out.println("wynik to " + response);
-       return response;
+
+    public static int getDivision(int a, int b) {
+        return a / b;
     }
 
 }
