01152791e9b9d09da68fc5e0e1b2c8daa2a94eae
Agnieszka Wiekiera <agnieszka.wiekiera97@gmail.com>
2023-01-09 18:25:36 +0100

    poprawiony kalkulator

--- mvnProject/src/main/java/org/example/Calculator.java
+++ mvnProject/src/main/java/org/example/Calculator.java
@@ -1,14 +1,16 @@
 package org.example;
+
 import java.util.Scanner;
 
 public class Calculator {
 
     public static int getSum(int a, int b) {
-        			return a + b;
-        		}
+        return a + b;
+    }
+
     public static int getSubtraction(int a, int b) {
-        			return a - b;
-        		}
+        return a - b;
+    }
 
     public static int getMultiplication(int a, int b) {
         return a * b;
